26d9889bc7efda4589b13705162f765cd8101091
James Lee <james@Jamess-iMac.local>
2021-10-05 22:29:04 -0400

    add buttons to scene

--- projects/aniText/aniText.js
+++ projects/aniText/aniText.js
@@ -1,13 +1,12 @@
-//do not support multiline atm
+//do not support multiline
+//do not support x2, y2 aka boundaries
 class AniText {
-    constructor(str, x1, y1, animation, length = 1000, x2 = null, y2 = null) {
+    constructor(str, x1, y1, animation, length = 1000) {
         this.animation = animation
         this.length = length
         this.str = str
         this.x1 = x1
         this.y1 = y1
-        this.x2 = x2
-        this.y2 = y2
         this.textWidth = textWidth(this.str)
         this.textHeight = this.getTextHeight(this.str)
         this.animationFinished = true
--- projects/aniText/sketch.js
+++ projects/aniText/sketch.js
@@ -2,27 +2,36 @@ let sel
 let t
 let str
 let canvas
+let button
 let selectList = ['slideInLeft', 'slideInRight', 'slideInUp', 'slideInDown']
 function setup () {
     canvas = createCanvas(windowWidth, windowHeight)
     canvas.position(0, 0)
     canvas.style('z-index', '-1')
 
-    rectMode(CENTER)
     sel = createSelect();
-    sel.position(windowWidth / 2 - 100, windowHeight / 6)
+    sel.position(windowWidth / 2 - 160, windowHeight / 6)
     selectList.forEach(d => {
         sel.option(d)
     })
     sel.selected(selectList[0]);
     sel.changed(changeAnimation);
-    textSize(48)
+
+    textSize(64)
     changeAnimation()
+
+    button = createButton('Repeat!')
+    button.position(windowWidth / 2 + 60, windowHeight / 6)
+    button.mousePressed(replayAnimation)
 }
 
 function changeAnimation () {
     let item = sel.value();
     t = new AniText(item, windowWidth / 2 - textWidth(item) / 2, windowHeight / 2 - 50, item)
+}
+
+function replayAnimation () {
+    t.start()
 }
 
 function windowResized () {
