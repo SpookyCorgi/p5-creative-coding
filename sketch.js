36d79af88c8239b5427f4bc78bd3028221fbf0a0
James Lee <james@Jamess-iMac.local>
2021-10-10 20:09:56 -0400

    add animation to main page background and fix unnecessary scrolling

--- sketch.js
+++ sketch.js
@@ -1,22 +1,29 @@
 let canvas
+let rot = []
+const count = 5
 function setup () {
     canvas = createCanvas(windowWidth * 2, windowHeight * 2, WEBGL);
-    noLoop()
     noFill()
     stroke("#ccffff66")
     strokeWeight(1)
     rectMode(CENTER)
     canvas.position(-windowWidth / 2, -windowHeight / 2)
     canvas.style('z-index', '-1')
+    for (let i = 0; i < count; i++) {
+        let v = createVector(random(PI * 2), random(PI * 2), random(PI * 2))
+        rot.push(v)
+    }
 }
 
-let count = 5
+
 function draw () {
     background(0);
-    for (let i = 0; i < count; i++) {
-        rotateX(random(PI * 2))
-        rotateY(random(PI * 2))
-        rotateZ(random(PI * 2))
+    rot.forEach(d => {
+        push()
+        rotateX(d.x + millis() / 100000)
+        rotateY(d.y + millis() / 200000)
+        rotateZ(d.z + millis() / 500000)
         rect(0, 0, width, height)
-    }
+        pop()
+    })
 }
