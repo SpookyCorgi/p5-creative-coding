41c6479ee48e0802c39a55e9e4ab6c5f6a433a64
James Lee <b05502067@ntu.edu.tw>
2021-10-28 00:36:46 -0400

    update to test

--- projects/test/sketch.js
+++ projects/test/sketch.js
@@ -16,35 +16,54 @@ let captureStartY = 0
 let captureScale = 1
 let captureWidth
 let captureHeight
+
+let div
+
+let myShader
+let matcap
+function preload () {
+    myShader = loadShader("shader.vert", "shader.frag");
+    matcap = loadImage("422509_C89536_824512_0A0604.png");
+}
+
 function setup () {
-    canvas = createCanvas(windowWidth, windowHeight)
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL)
     canvas.position(0, 0)
     canvas.style('z-index', '-1')
 
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight)
 
+    div = createDiv('');
+    createCheckboxes()
+
+    //capture and scale
+    capture = createCapture(VIDEO)
+    capture.hide()
+    scaling()
+    noStroke()
+    rectMode(CENTER)
+}
+
+function createCheckboxes () {
+    div.remove()
+    div = createDiv('');
+    checkboxes = []
     //checkboxes
     hAmount = int(windowWidth / gridSize)
     vAmount = int((windowHeight - paddingTop) / gridSize)
     hPadding = windowWidth % gridSize / 2
     vPadding = (windowHeight - paddingTop) % gridSize / 2
-    for (let j = 0; j < vAmount; j++) {
+    /*for (let j = 0; j < vAmount; j++) {
         for (let i = 0; i < hAmount; i++) {
-            let checkbox = createCheckbox('', true);
+            let checkbox = createCheckbox('', true).parent(div)
             checkbox.position(hPadding + i * gridSize, paddingTop + vPadding + j * gridSize);
             checkboxes.push(checkbox)
         }
-    }
-
-    //capture and scale
-    capture = createCapture(VIDEO);
-    capture.hide()
-    scaling()
-    noStroke()
+    }*/
 }
-
 function windowResized () {
     resizeCanvas(windowWidth, windowHeight)
+    createCheckboxes()
 }
 
 function scaling () {
@@ -65,27 +84,33 @@ function scaling () {
 }
 
 function draw () {
-    background(255)
+    shader(myShader)
+    myShader.setUniform("uMatcapTexture", matcap)
+    background(0)
     scaling()
-    //console.log(captureScale, hAmount, vAmount, captureWidth, captureHeight)
     capture.loadPixels()
-
     for (let j = 0; j < vAmount; j++) {
         for (let i = 0; i < hAmount; i++) {
             let start = (captureStartY * capture.width + captureStartX) * 4
             let p = start + (j * capture.width * captureScale + i * captureScale) * 4
-            console.log(start, p)
             r = capture.pixels[p]
             g = capture.pixels[p + 1]
             b = capture.pixels[p + 2]
-            let brightness = (r * 2 + g * 3 + b) / 6
+            let brightness = 0.2126 * r + 0.7152 * g + 0.0722 * b
             if (brightness > brightnessThreshold) {
-                checkboxes[j * hAmount + i].checked(false)
+                //checkboxes[j * hAmount + i].checked(false)
             } else {
-                checkboxes[j * hAmount + i].checked(true)
+                //checkboxes[j * hAmount + i].checked(true)
             }
-            fill(color(r, g, b))
-            //circle(hPadding + i * gridSize, paddingTop + vPadding + j * gridSize, 20)
+            push()
+            translate(hPadding + i * gridSize + gridSize / 2 - windowWidth / 2,
+                paddingTop + vPadding + j * gridSize + gridSize / 2 - windowHeight / 2)
+            rotateX((255 - brightness) / 255 * PI)
+            //scale((255 - brightness) / 255)
+            //fill(0)
+            //rect(0, 0, gridSize, gridSize)
+            torus(6, 5)
+            pop()
         }
     }
     capture.updatePixels()
